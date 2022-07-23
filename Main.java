bb45499b3cadc08aada6c1c300f1bee02569de75
BraianContrerasM <109833060+BraianContrerasM@users.noreply.github.com>
2022-07-22 18:46:47 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package Paquete;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("                  __________(^-^)__________           ");
+        System.out.println( "Bienvenido a progra 2 2022: "+" Jesus Braian Contreras Mazariegos.");
+        System.out.println("___________________________________________________________________");
+    }
+}
